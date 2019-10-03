8cf9ca4e605a7a1ce169dee4b82e89e6712f2576
vpilly <varun.pilly@gmail.com>
2019-10-03 19:13:44 -0400

    added main.catch

--- inclass2/app.js
+++ inclass2/app.js
@@ -2,26 +2,22 @@ const todoItems = require("./todo");
 const connection = require("./mongoConnection");
 
 async function main() {
-    try {
-        const createdTask = await todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?");
-        console.log(createdTask);
-        const id1 = String(createdTask['_id']);
-        const secondTask = await todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive Helix?");
-        const id2 = String(secondTask['_id']);
-        console.log();
-        console.log(await todoItems.getAllTasks());
-        console.log();
-        await todoItems.removeTask(id1);
-        console.log(await todoItems.getAllTasks());
-        console.log();
-        console.log(await todoItems.completeTask(id2));
-    } catch (error) {
-        console.log(error);
-    }
-
+    const createdTask = await todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?");
+    console.log(createdTask);
+    const id1 = String(createdTask['_id']);
+    const secondTask = await todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive Helix?");
+    const id2 = String(secondTask['_id']);
+    console.log();
+    console.log(await todoItems.getAllTasks());
+    console.log();
+    await todoItems.removeTask(id1);
+    console.log(await todoItems.getAllTasks());
+    console.log();
+    console.log(await todoItems.completeTask(id2));
+    
     const db = await connection();
     await db.serverConfig.close();
     console.log("finished");
 }
 
-main();
+main().catch();
